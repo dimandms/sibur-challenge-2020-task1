b3c7bec95de3b8d40a74966cf212ca1f4474f662
Богданов Дмитрий Сергеевич <dbogdanov@en-pro.ru>
2020-11-24 15:45:15 +0300

    submission mape=6.9196

--- metrics.py
+++ metrics.py
@@ -2,7 +2,8 @@ import numpy as np
 
 
 def mean_absolute_percentage_error(y_true, y_pred):
-    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+    mask = y_true != 0.0
+    return np.mean(np.abs((y_true[mask] - y_pred[mask]) / y_true[mask])) * 100
 
 
 def absolute_errors(y_true, y_pred):
